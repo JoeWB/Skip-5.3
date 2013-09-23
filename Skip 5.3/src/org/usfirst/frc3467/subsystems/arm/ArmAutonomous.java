ef238cd3159c207fcda2447ab9b4623edd9720d5
nhebert16 <nhebert16@gmail.com>
2013-09-23 15:05:56 -0700

    Added arm Subsystem and classes
    class for the arm subsytem were made: ArmManual and ArmAutonomus

---
+++ Skip 5.3/src/org/usfirst/frc3467/subsystems/arm/ArmAutonomous.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc3467.subsystems.arm;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ArmAutonomous extends Subsystem{
+
+	
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ Skip 5.3/src/org/usfirst/frc3467/subsystems/arm/ArmManual.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc3467.subsystems.arm;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ArmManual extends Subsystem{
+
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
