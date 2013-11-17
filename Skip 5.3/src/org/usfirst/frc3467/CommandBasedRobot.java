174c557c845b1b6517b3eb9a39e5aeeef855e283
Jake Mathews <JakeMathews@users.noreply.github.com>
2013-11-17 10:03:12 -0500

    Update CommandBasedRobot.java

--- Skip 5.3/src/org/usfirst/frc3467/CommandBasedRobot.java
+++ Skip 5.3/src/org/usfirst/frc3467/CommandBasedRobot.java
@@ -26,7 +26,8 @@ public class CommandBasedRobot extends IterativeRobot {
 	/**
 	 * This function is run when the robot is first started up and should be used for any initialization code.
 	 */
-	// A test of source tree
+	// Well well welll
+	//What do we have here?
 	public void robotInit() {
 		// instantiate the command used for the autonomous period
 		// autonomousCommand = new Auto();
